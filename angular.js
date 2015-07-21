92f36b05f4f01ea77658bb2ee8be399ec235afe1
Yuliya Kaleda <yulia.koleda1988@gmail.com>
2015-07-21 02:37:13 -0400

    Update angular.js

--- angular.js
+++ angular.js
@@ -11,9 +11,14 @@ app.directive('mirror', function(MirrorLink, mirrorTemplateUrl) {
 
 app.value('mirrorTemplateUrl', 'mirror.html');
 
-app.service("MirrorLink", function(GumHandlers) {
+app.service("MirrorLink", function(GumHandlers, animateFunction) {
+	var element = $(element);
+
 	return function(scope, element, attributes, parent) {
 		console.log("Mirror linking function initialized!");
+		
+		//element = $(element);
+
 		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 		window.URL = window.URL || window.webkitURL;
 		
@@ -25,7 +30,83 @@ app.service("MirrorLink", function(GumHandlers) {
 		}
 		
 		navigator.getUserMedia({video: true}, GumHandlers.gotStream.bind(null, camvideo), GumHandlers.noStream);		
+
+		/* The code below contains a loop to draw the contents of the video tag onto the canvas tag, enabling us to do cool things with the image. */
+
+		// var video = element.children('#monitor')[0];
+		var videoCanvas = element.find( '#videoCanvas' )[0];
+		var videoContext = videoCanvas.getContext( '2d' );
+
+		var layer2Canvas = element.find( '#layer2' )[0];
+		var layer2Context = layer2Canvas.getContext( '2d' );
+
+		var blendCanvas  = element.find("#blendCanvas" )[0];
+		var blendContext = blendCanvas.getContext('2d');
+
+		videoContext.translate(1080, 0);
+		videoContext.scale(-1, 1);
+
+		// //text on video
+		layer2Context.font = "80px Helvetiker";
+		layer2Context.fillText("WE LOVE MTA =)",430,100);
+				
+		// background color if no video present
+		videoContext.fillStyle = '#000000';
+		videoContext.fillRect( 0, 0, videoCanvas.width, videoCanvas.height );				
+
+		var buttons = [];
+
+		var button1 = new Image();
+		button1.src ="img/redCircle.png";
+		var buttonData1 = { name:"red", image:button1, x:320 - 96 - 160, y:40, w:72, h:72 };
+		buttons.push( buttonData1 );
+
+		var button2 = new Image();
+		button2.src ="img/greenCircle.png";
+		var buttonData2 = { name:"green", image:button2, x:320 - 64 - 100, y:40, w:72, h:72 };
+		buttons.push( buttonData2 );
+
+		var button3 = new Image();
+		button3.src ="img/blueCircle.png";
+		var buttonData3 = { name:"blue", image:button3, x:320 - 32-40, y:40, w:72, h:72 };
+		buttons.push( buttonData3 );
+
+		animateFunction(camvideo, videoContext, layer2Context, buttons, videoCanvas, blendContext, null);		
 	};
+});
+
+app.service('computeDifference', function() {
+	function differenceAccuracy(target, data1, data2) 
+	{
+		if (data1.length !== data2.length) 
+			{
+				return null;
+			}
+		var i = 0;
+		while (i < (data1.length * 0.25)) 
+		{
+			var average1 = (data1[4*i] + data1[4*i+1] + data1[4*i+2]) / 3;
+			var average2 = (data2[4*i] + data2[4*i+1] + data2[4*i+2]) / 3;
+			var diff = threshold(fastAbs(average1 - average2));
+			target[4*i]   = diff;
+			target[4*i+1] = diff;
+			target[4*i+2] = diff;
+			target[4*i+3] = 0xFF;
+			++i;
+		}
+	}
+
+	function fastAbs(value) 
+	{
+		return (value ^ (value >> 31)) - (value >> 31);
+	}
+
+	function threshold(value) 
+	{
+		return (value > 0x15) ? 0xFF : 0;
+	}
+
+	return differenceAccuracy;
 });
 
 app.service('GumHandlers', function() {
@@ -55,123 +136,44 @@ app.service('GumHandlers', function() {
 	};
 });
 
-/*
+app.service('blendFunction', function(computeDifference) {
+	return function blend(videoCanvas, blendContext, lastImageData) {
+		var videoContext = videoCanvas.getContext('2d');
+		var width  = videoCanvas.width;
+		var height = videoCanvas.height;
+		// get current webcam image data
+		var sourceData = videoContext.getImageData(0, 0, width, height);
+		// create an image if the previous image doesn't exist
+		if (!lastImageData) 
+			{
+				lastImageData = videoContext.getImageData(0, 0, width, height);
+			}
+		// create an ImageData instance to receive the blended result
+		var blendedData = videoContext.createImageData(width, height);
+		// blend the 2 images
+		computeDifference(blendedData.data, sourceData.data, lastImageData.data);
+		// draw the result in a canvas
+		blendContext.putImageData(blendedData, 0, 0);
+		// store the current webcam image
+		return sourceData;
+	};
+});
 
-<!-- The code below contains a loop to draw the contents of the video tag
-	 onto the canvas tag, enabling us to do cool things with the image. -->
-
-
-var video = document.getElementById( 'monitor' );
-var videoCanvas = document.getElementById( 'videoCanvas' );
-var videoContext = videoCanvas.getContext( '2d' );
-
-var layer2Canvas = document.getElementById( 'layer2' );
-var layer2Context = layer2Canvas.getContext( '2d' );
-
-var blendCanvas  = document.getElementById( "blendCanvas" );
-var blendContext = blendCanvas.getContext('2d');
-
-videoContext.translate(1080, 0);
-videoContext.scale(-1, 1);
-
-//text on video
-layer2Context.font = "80px Helvetiker";
-layer2Context.fillText("WE LOVE MTA =)",430,100);
-		
-// background color if no video present
-videoContext.fillStyle = '#000000';
-videoContext.fillRect( 0, 0, videoCanvas.width, videoCanvas.height );				
-
-var buttons = [];
-
-var button1 = new Image();
-button1.src ="img/redCircle.png";
-var buttonData1 = { name:"red", image:button1, x:320 - 96 - 160, y:40, w:72, h:72 };
-buttons.push( buttonData1 );
-
-var button2 = new Image();
-button2.src ="img/greenCircle.png";
-var buttonData2 = { name:"green", image:button2, x:320 - 64 - 100, y:40, w:72, h:72 };
-buttons.push( buttonData2 );
-
-var button3 = new Image();
-button3.src ="img/blueCircle.png";
-var buttonData3 = { name:"blue", image:button3, x:320 - 32-40, y:40, w:72, h:72 };
-buttons.push( buttonData3 );
-
-// start the loop				
-animate();
-
-function animate() 
-{
-    requestAnimationFrame( animate );
-	
-	render();	
-	blend();	
-	checkAreas();
-}
-
-function render() 
-{	
-	if ( video.readyState === video.HAVE_ENOUGH_DATA ) 
+app.value('renderFunction', function render(camvideo, videoContext, layer2Context, buttons){
+	if ( camvideo.readyState === camvideo.HAVE_ENOUGH_DATA ) 
 	{
 		// mirror video
-		videoContext.drawImage( video, 0, 0, videoCanvas.width, videoCanvas.height );
+		videoContext.drawImage( camvideo, 0, 0, videoCanvas.width, videoCanvas.height );
 		for ( var i = 0; i < buttons.length; i++ )
+		{
 			layer2Context.drawImage( buttons[i].image, buttons[i].x, buttons[i].y, buttons[i].w, buttons[i].h );		
+		}		
 	}
-}
+});
 
-var lastImageData;
-
-function blend() 
-{
-	var width  = videoCanvas.width;
-	var height = videoCanvas.height;
-	// get current webcam image data
-	var sourceData = videoContext.getImageData(0, 0, width, height);
-	// create an image if the previous image doesn't exist
-	if (!lastImageData) lastImageData = videoContext.getImageData(0, 0, width, height);
-	// create an ImageData instance to receive the blended result
-	var blendedData = videoContext.createImageData(width, height);
-	// blend the 2 images
-	differenceAccuracy(blendedData.data, sourceData.data, lastImageData.data);
-	// draw the result in a canvas
-	blendContext.putImageData(blendedData, 0, 0);
-	// store the current webcam image
-	lastImageData = sourceData;
-}
-
-function differenceAccuracy(target, data1, data2) 
-{
-	if (data1.length != data2.length) return null;
-	var i = 0;
-	while (i < (data1.length * 0.25)) 
-	{
-		var average1 = (data1[4*i] + data1[4*i+1] + data1[4*i+2]) / 3;
-		var average2 = (data2[4*i] + data2[4*i+1] + data2[4*i+2]) / 3;
-		var diff = threshold(fastAbs(average1 - average2));
-		target[4*i]   = diff;
-		target[4*i+1] = diff;
-		target[4*i+2] = diff;
-		target[4*i+3] = 0xFF;
-		++i;
-	}
-}
-
-function fastAbs(value) 
-{
-	return (value ^ (value >> 31)) - (value >> 31);
-}
-
-function threshold(value) 
-{
-	return (value > 0x15) ? 0xFF : 0;
-}
 
 // check if white region from blend overlaps area of interest (e.g. triggers)
-function checkAreas() 
-{
+app.value('checkAreasFunction', function checkAreas(buttons, blendContext, layer2Context){
 	for (var b = 0; b < buttons.length; b++)
 	{
 		// get the pixels in a note area from the blended image
@@ -191,22 +193,35 @@ function checkAreas()
 		if (average > 50) // more than 20% movement detected
 		{
 			console.log( "Button " + buttons[b].name + " triggered." ); // do stuff
-			if (buttons[b].name == "red") {
+			if (buttons[b].name === "red") {
 				layer2Context.fillStyle = "red";
 				layer2Context.font = "80px Helvetiker";
 				layer2Context.fillText("WE LOVE MTA =)",430,100);	
-		}
-			if (buttons[b].name == "green") {
+			}
+			if (buttons[b].name === "green") {
 				layer2Context.fillStyle = "green";
 				layer2Context.font = "80px Helvetiker";
 				layer2Context.fillText("WE LOVE MTA =)",430,100);
-		}
-			if (buttons[b].name == "blue") {
+			}
+			if (buttons[b].name === "blue") {
 				layer2Context.fillStyle = "blue";
 				layer2Context.font = "80px Helvetiker";
 				layer2Context.fillText("WE LOVE MTA =)",430,100);
 			}
 		}
 	}
-}
-*/
+});
+
+app.service('animateFunction', function (renderFunction, blendFunction, checkAreasFunction){
+	var lastImageData, renderObject, blendedImage;
+
+	function animate(camvideo, videoContext, layer2Context, buttons, videoCanvas, blendContext, lastImageData) {
+		renderFunction(camvideo, videoContext, layer2Context, buttons);
+		checkAreasFunction(buttons, blendContext, layer2Context);		
+		lastImageData = blendFunction(videoCanvas, blendContext, lastImageData);
+
+		requestAnimationFrame( animate.bind(this, camvideo, videoContext, layer2Context, buttons, videoCanvas, blendContext, lastImageData) );
+	}
+
+	return animate;
+});
